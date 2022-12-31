346810fa44590ca20e4b49daa2389909b3e531e2
MilanS3333 <110888010+MilanS3333@users.noreply.github.com>
2022-12-31 09:10:15 +0530

    website

---
+++ paint web-site  (not-responsive)/js/js.js
@@ -0,0 +1,39 @@
+// slider code
+var swiper = new Swiper('.swiper-container',{
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    }
+});
+
+// fade effect code
+window.onload = function () {
+    lax.init()
+
+    // Add a driver that we use to control our animations
+    lax.addDriver('scrollY', function () {
+      return window.scrollY
+    })
+
+    // Add animation bindings to elements
+    lax.addElements('.selector', {
+      scrollY: {
+        translateX: [
+          ["elInY", "elCenterY", "elOutY"],
+          [0, 'screenWidth/2', 'screenWidth'],
+        ]
+      }
+    })
+  }
